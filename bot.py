340102e90a8f4c4eeff8380434251f810f981678
AllNicknamesAreTaken <mat3usz268@gmail.com>
2023-01-07 22:18:01 +0100

    Sending messages

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,4 +22,25 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    print(message)
+    print(message.content)
+    
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    
+        
 client.run(TOKEN)
